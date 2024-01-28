6656302a3fed88199bb041bb856d3aaec86fa90b
marwan-gama <marwanabugama2000@gmail.com>
2024-01-28 10:52:12 +0200

    add displayMessage after delete

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const temp = document.querySelector(".btn");
 
-temp.addEventListener("click", function () {
+temp.addEventListener("click", function() {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+// Function to display the message after a short delay
+function displayMessage() {
+  var message = document.getElementById("downloadMessage");
+  message.style.display = "block";
+}
+
+// Add an event listener to the download link
+document
+  .getElementById("downloadLink")
+  .addEventListener("click", function(event) {
+    setTimeout(displayMessage, 1000);
+  });
