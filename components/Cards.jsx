3eaca686c47a8c17b7e89aa6e94b6b9c1e7d0a32
AreenAl <areenaldda@gmail.com>
2024-01-29 15:51:30 +0200

    adding jsx file for the home page

---
+++ components/Cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Cards = () => {
+  return (
+      <div> aaa </div>
+  )
+}
+
+export default Cards
---
+++ pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from '../components/Cards'
+
+
+const HomePage = () => {
+  return (
+  <>
+      <div> home </div>
+      <div> <Cards /> </div>
+      </>
+  );
+}
+
+export default HomePage;
