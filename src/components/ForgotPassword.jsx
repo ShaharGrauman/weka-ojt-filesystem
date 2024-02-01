84c924fbb7825efda1bfbf1a34ce9a92b240b22c
e5lassnb <131860041+ekhlassnbary@users.noreply.github.com>
2024-02-01 16:23:39 +0200

    add component for forgot password

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react'
+
+
+function ForgotPassword() {
+  return(
+     <div className="row shadow border border-2 justify-content-center justify-content-lg-around  m-5 " style={{backgroundColor: '#EDEDED'}}>
+        <h1 className="text-center m-1">Forget password</h1>
+        <div className="form-group text-start ">
+          <label htmlFor="Email"> Email address</label>
+          <input type="email" className="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter your email address" />
+        </div>
+        <div>
+          {/*              <small><a class="text-sm-end m-1" href="#"><span>Forget Password</span></a></small>*/}
+          <small><a className="text-sm-start m-1 t" href="#"><span>SignUp</span></a></small>
+        </div>
+        <button type="submit" className="btn btn-primary">confirm</button>
+      </div>
+
+  )
+}
