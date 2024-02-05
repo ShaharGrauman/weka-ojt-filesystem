5f1acf053a820df61cb750c1124a50272babe0a9
marwan-gama <marwanabugama2000@gmail.com>
2024-02-05 13:27:52 +0200

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import "./App.css"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
-import LogIn from "./pages/Login.jsx"
+import LogIn from "./pages/Login.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
-  <div>
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<LogIn />} />
           <Route path="/homepage" element={<HomePage />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
-      </div>
-  )
+    </div>
+  );
 }
 
 export default App;
