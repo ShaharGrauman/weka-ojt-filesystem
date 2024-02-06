ac73d4772023d05ceb4b10a8bd2c65bc7d79480a
AreenAl <areenaldda@gmail.com>
2024-02-06 11:04:27 +0200

    show details for item when clicked

--- src/components/ItemDropDown.jsx
+++ src/components/ItemDropDown.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import Dropdown from "./dropdown";
+import React, { useState } from "react";
+import Dropdown from "./Dropdown"; // Make sure to import Dropdown component properly
 import Share from "./Share";
 import FileDetailsModal from "./details"
 
-const HomeDropdown = ({ selectedItem }) => {
+const ItemDropDown = ({ selectedItem }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   const homeOptions = [
     { value: "download", label: "Download" },
     { value: "move", label: "Move" },
@@ -16,21 +18,27 @@ const HomeDropdown = ({ selectedItem }) => {
 
   const handleOptionSelect = (selectedOption) => {
     if (selectedOption.value === "details") {
+      setShowDropdown(false); // Hide the dropdown menu after selecting an option
       return <FileDetailsModal showModal={true} onClose={() => {}} fileDetails={selectedItem} />;
       // Handle details option
     } else if (selectedOption === "share") {
+        setShowDropdown(false); // Hide the dropdown menu after selecting an option
       // Handle share option
       console.log("Sharing...", selectedItem);
     }
   };
 
   return (
+  <div>
     <Dropdown
       options={homeOptions}
       onSelect={handleOptionSelect}
       plusIcon={false}
+      showDropdown={showDropdown}
+      toggleDropdown={() => setShowDropdown(!showDropdown)}
    />
+  </div>
   );
 };
 
-export default HomeDropdown;
+export default ItemDropDown;
