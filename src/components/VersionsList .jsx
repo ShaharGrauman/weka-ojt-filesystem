3c0be7fb80b5bdd8f9fa4870a21dac3a4cfbb968
e5lassnb <131860041+ekhlassnbary@users.noreply.github.com>
2024-02-11 14:56:29 +0200

    fix virsion

--- src/components/VersionsList .jsx
+++ src/components/VersionsList .jsx
@@ -1,26 +1,17 @@
 
 import React from 'react';
 import FileVersion from './versionsFile';
+import VersionsDropdown from './VersionsDropdown';
 
 const VersionsList = ({ versionData }) => (
-
-//  const { updateState } = show;
-//   const gotologin = () => {
-//     // Your conditions here
-//     updateState(false, true); // Example values, update as needed
-//   };
-
-
   <div className="container mt-5">
     <h1 className="text-center mb-5">ojt.txt- Versions</h1>
-
     {versionData.map((version, index) => (
       <FileVersion
         key={index} // Ensure each child component has a unique key
         number={version.number}
         date={version.date}
-        icon={<i className="lni lni-empty-file custom-icon"></i>} 
-        dropdownItems={version.dropdownItems}
+        icon={<i className="lni lni-empty-file custom-icon"></i>}
       />
     ))}
   </div>
