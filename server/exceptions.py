111a114f63880cabe7610003e4b848034b894a16
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2024-03-03 21:59:14 +0200

    add custom exception

--- server/exceptions.py
+++ server/exceptions.py
@@ -1,12 +1,6 @@
 from fastapi import HTTPException
-from datetime import datetime
+
 
 class CustomHTTPException(HTTPException):
-    def __init__(self, status_code: int = 400, detail: dict = None):
-        self.status_code = status_code
-        self.detail = detail or {
-            "status": status_code,
-            "message": "An error occurred",
-            "date": datetime.now().isoformat()
-        }
-        super().__init__(status_code=status_code, detail=self.detail)
+    def __init__(self, status_code: int = 403, detail: str = "Forbidden: User is not an admin"):
+        super().__init__(status_code=status_code, detail=detail)
