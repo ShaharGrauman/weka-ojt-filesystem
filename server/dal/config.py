cfa03c9cc7f1912a6030fd6779e35b0ae9a01a14
Your GitHub Username <marwanabugama2000@gmail.com>
2024-03-04 13:41:06 +0200

    add git ignore

--- server/dal/config.py
+++ server/dal/config.py
@@ -1,11 +1,10 @@
 from cryptography.fernet import Fernet
 
-host="localhost"
-user="root"
-password="Hamad12345"
-database="filesystem"
+host = 'localhost'
+user = 'root'
+password = '1234'
+database = 'filesystem'
 port = 3306
-
 
 encryption_key = b'BpthmmKROjL-bMMnZD8h-jye-ZJN6cY7z-QB3ms_qD0='
 cipher = Fernet(encryption_key)
