380cad399017649dd2ab8aff7b428b074da0262d
ekhlass <131860041+ekhlassnbary@users.noreply.github.com>
2024-03-04 11:36:41 +0200

    send email

--- server/dal/dalFuction.py
+++ server/dal/dalFuction.py
@@ -1,4 +1,7 @@
-
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
 from cryptography.fernet import Fernet
 
 encryption_key = b'BpthmmKROjL-bMMnZD8h-jye-ZJN6cY7z-QB3ms_qD0='
@@ -9,5 +12,28 @@ cipher = Fernet(encryption_key)
 def Encrypt_email(email):
    encrypted_email= cipher.encrypt(email.encode())
    return encrypted_email
+
+# send email to the user
+email_sender="filesystem2024@gmail.com"
+email_password="ejnw zjwu gmfc jzjt"
+email_receiver="ekhlass@post.bgu.ac.il"
+
+subject='check out mu new vedii'
+body=""""
+ifcde niehd ihfic"""
+
+em =EmailMessage()
+em['From']=email_sender
+em['To']= email_receiver
+em['Subject']=subject
+em.set_content(body) 
+
+
+context=ssl.create_default_context()
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context)as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender,email_receiver,em.as_string())
 
 
