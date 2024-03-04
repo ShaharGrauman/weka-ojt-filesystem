414ebaa1d3ea94ada0d337ac73d535773cf8a215
AhatSaraih <ahat.saraiah@gmail.com>
2024-03-04 13:53:38 +0200

    add restore functions

--- server/dal/deleted_options.py
+++ server/dal/deleted_options.py
@@ -5,7 +5,7 @@ def permanently_delete_file(file_id,user_id):
 
     connection = get_database_connection()
     cursor = connection.cursor()
-    delete_query = "DELETE FROM files WHERE file_id=? AND user_id=?;"
+    delete_query = "DELETE FROM files WHERE id=? AND user_id=?;"
 
     try:
         cursor.execute(delete_query, (file_id, user_id))
@@ -28,7 +28,7 @@ def permanently_delete_folder(folder_id,user_id):
     cursor = connection.cursor()
     delete_files= "DELETE FROM files WHERE parent_id=? AND user_id=?;"
     delete_folders= "DELETE FROM folders WHERE parent_id=? AND user_id=?;"
-    delete_query = "DELETE FROM folders WHERE folder_id=? AND user_id=?;"
+    delete_query = "DELETE FROM folders WHERE id=? AND user_id=?;"
 
     try:
         cursor.execute(delete_files, (folder_id, user_id))
@@ -39,6 +39,49 @@ def permanently_delete_folder(folder_id,user_id):
         return {
             "status": "success",
             "msg": f"Folder with ID {folder_id} permanently deleted."
+        }
+    except Exception as e:
+        connection.rollback()
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        cursor.close()
+
+
+def restore_file(file_id,user_id):
+
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    restore_query = "UPDATE files SET is_deleted = %s WHERE id = %sAND user_id=?;"
+
+    try:
+        cursor.execute(restore_query, ("0",file_id, user_id))
+        connection.commit()
+
+        return {
+            "status": "success",
+            "msg": f"File with ID {file_id} restored successfully."
+        }
+    except Exception as e:
+        connection.rollback()
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        cursor.close()
+
+
+
+def restore_folder(folder_id,user_id):
+
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    restore_query = "UPDATE folders SET is_deleted = %s WHERE id = %sAND user_id=?;"
+
+    try:
+        cursor.execute(restore_query, ("0",folder_id, user_id))
+        connection.commit()
+
+        return {
+            "status": "success",
+            "msg": f"Folder with ID {folder_id} restored successfully."
         }
     except Exception as e:
         connection.rollback()
--- server/routes/deleted_route.py
+++ server/routes/deleted_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from server.dal.deleted_options import permanently_delete_file,permanently_delete_folder
+from server.dal.deleted_options import permanently_delete_file,permanently_delete_folder,restore_file,restore_folder
 from server.exceptions import CustomHTTPException
 
 
@@ -27,4 +27,29 @@ def delete_folder(folder_id: int,user_id:int):
         return e
     except Exception as e:
         raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+
+
+@router.update("/deleted/files/{file_id}/restore")
+def file_restore(file_id: int,user_id:int):
+    try:
+        # Check if file restored
+        result = restore_file( file_id,user_id)
+        return result
+    except CustomHTTPException as e:
+        return e
+    except Exception as e:
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    
+@router.update("/deleted/folders/{folder_id}/restore")
+def folder_restore(folder_id: int,user_id:int):
+    try:
+        # Check if folder restored
+        result = restore_folder( folder_id,user_id)
+        return result
+    except CustomHTTPException as e:
+        return e
+    except Exception as e:
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
 
