c2cfefca6fdf2b8274a7cb92f34a7643a74f0774
AhatSaraih <ahat.saraiah@gmail.com>
2024-03-04 14:27:08 +0200

    add delete file / folder to threedots route

--- server/dal/threeDots.py
+++ server/dal/threeDots.py
@@ -1,12 +1,11 @@
 from dal.mysql_connection import get_database_connection
-
+from dal.mysql_connection import get_database_connection
+from exceptions import CustomHTTPException
 
 connection = get_database_connection()
 
-
-
 def get_versions_for_file(file_id):
-    conn = get_database_connection()  # Assuming get_database_connection() returns a valid database connection
+    conn = get_database_connection()  
     cursor = conn.cursor()
 
     # Fetch versions for the given file_id using a single query
@@ -21,3 +20,57 @@ def get_versions_for_file(file_id):
     conn.close()
 
     return versions
+
+
+
+def delete_file(file_id,user_id):
+
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    delete_query = update_is_deleted_file()
+
+    try:
+        cursor.execute(delete_query, ("1",file_id, user_id))
+        connection.commit()
+
+        return {
+            "status": "success",
+            "msg": f"File with ID {file_id} deleted successfully."
+        }
+    except Exception as e:
+        connection.rollback()
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        cursor.close()
+
+
+def delete_folder(folder_id,user_id):
+
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    delete_query = update_is_deleted_folder()
+
+    try:
+        cursor.execute(delete_query, ("1",folder_id, user_id))
+        connection.commit()
+
+        return {
+            "status": "success",
+            "msg": f"Folder with ID {folder_id} deleted successfully."
+        }
+    except Exception as e:
+        connection.rollback()
+        raise CustomHTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        cursor.close()
+
+
+
+
+def update_is_deleted_file():
+    restore_query = "UPDATE files SET is_deleted = %s WHERE id = %sAND user_id=?;"
+    return restore_query       
+
+def update_is_deleted_folder():
+    restore_query = "UPDATE folders SET is_deleted = %s WHERE id = %sAND user_id=?;"
+    return restore_query
