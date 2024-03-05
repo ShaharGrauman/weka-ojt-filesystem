80a0354f65d8a4c60fff5af0548b8d51e8129167
AhatSaraih <ahat.saraiah@gmail.com>
2024-03-05 12:24:59 +0200

    connect restorefile to server

--- client/src/components/DeletedDropdown.jsx
+++ client/src/components/DeletedDropdown.jsx
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
-import Dropdown from './dropdown';
-import DeleteModal from "./DeletModal";
+import React, { useState } from "react";
+import Dropdown from "./dropdown";
+import DeletModal from "./DeletModal";
 import RestoreModal from "./RestoreModal";
 
-
-const DeletedDropdown = ({ selectedItem }) => {
+const DeletedDropdown = ({ selectedItem, userId }) => {
   const deletedOptions = [
-    { value: 'restore', label: 'Restore' },
-    { value: 'delete', label: 'Delete' },
+    { value: "restore", label: "Restore" },
+    { value: "delete", label: "Delete" },
   ];
   const [showDelete, setShowDelete] = useState(false);
   const [showRestore, setShowRestore] = useState(false);
 
-
   const handleOptionSelect = (selectedOption) => {
     switch (selectedOption.value) {
-      case 'restore':
-        console.log('Restoring item:', selectedItem);
+      case "restore":
+        console.log("Restoring item:", selectedItem);
         setShowRestore(true);
-      case 'delete':
-        console.log('Permanently deleting item:', selectedItem);
+      case "delete":
+        console.log("Permanently deleting item:", selectedItem);
         setShowDelete(true);
       default:
         break;
@@ -30,15 +28,29 @@ const DeletedDropdown = ({ selectedItem }) => {
     setShowRestore(false);
   };
 
-    return (
-        <div>
-        <Dropdown options={deletedOptions} onSelect={handleOptionSelect} plusIcon={false} />
-
-          {showDelete && <DeleteModal onClose={handleCloseModal} />}
-          {showRestore && <RestoreModal onClose={handleCloseModal} />}
-
-        </div>
-    )
+  return (
+    <div>
+      <Dropdown
+        options={deletedOptions}
+        onSelect={handleOptionSelect}
+        plusIcon={false}
+      />
+      {showDelete && (
+        <DeletModal
+          onClose={handleCloseModal}
+          itemId={selectedItem.id}
+          userId={userId}
+        />
+      )}
+      {showRestore && (
+        <RestoreModal
+          onClose={handleCloseModal}
+          itemId={selectedItem.id}
+          userId={userId}
+        />
+      )}
+    </div>
+  );
 };
 
 export default DeletedDropdown;
