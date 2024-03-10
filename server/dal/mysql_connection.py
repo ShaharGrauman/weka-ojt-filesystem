9e9a6910c853057f0f4c4dbe2993df38875bdfa2
AhatSaraih <ahat.saraiah@gmail.com>
2024-03-10 14:31:06 +0200

    update mysql conn

--- server/dal/mysql_connection.py
+++ server/dal/mysql_connection.py
@@ -16,31 +16,3 @@ def get_database_connection():
     else:
         print("Failed to connect to MySQL database")
     return connection
-
-
-# import mysql.connector
-# from mysql.connector import pooling
-# import mysql_details
-
-# # Set your database connection details
-# db_config = {
-# "host": "localhost",
-# "user": mysql_details.user,
-# "password": mysql_details.password,
-# "port": '3306',
-# "database": 'filesystem',
-# }
-
-# # Create a connection pool
-# connection_pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=10, **db_config)
-
-# def get_database_connection():
-# # Get a connection from the pool
-#     connection = connection_pool.get_connection()
-
-#     if connection.is_connected():
-#         print("Connected to MySQL database")
-#     else:
-#         print("Failed to get a connection from the pool")
-#     return connection
-
